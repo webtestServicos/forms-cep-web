83bfff329d7ca7bea203d7144e48145152f9c2ce
webtest <webtestservicos@gmail.com>
2025-07-31 12:09:48 -0300

    update the case-sensitive in build

--- src/sections/sectionQuery/SectionQuery.jsx
+++ src/sections/sectionQuery/SectionQuery.jsx
@@ -1,5 +1,5 @@
 import SidebarComponent from "../../components/sidebar/Sidebar.jsx";
-import QueryCustomer from "../../components/queryCustomers/queryCustomer.jsx";
+import QueryCustomer from "../../components/queryCustomers/QueryCustomer.jsx";
 import './SectionQuery.css';
 
 export default function SectionQuery() {
